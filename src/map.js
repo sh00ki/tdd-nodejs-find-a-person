655682b1d00fb42bad992f560ae3e613a119b420
Yoad Shiran <shooki@gmail.com>
2016-06-05 22:18:14 +0300

    restore map.js

--- src/map.js
+++ src/map.js
@@ -3,8 +3,34 @@ function Map(posts) {
   this._posts = posts;
 }
 
-Map.prototype.find_a_person = function(name) {
-  return [];
+Map.prototype.LookingForPerson = function(name) {
+  	var posts_contain_person_name = [];
+    for(i=0; i<this._posts.length; i++)
+ 	{
+ 		if(this._posts[i].includes(name))
+ 			posts_contain_person_name.push(this._posts[i]);
+ 	}
+ 	return posts_contain_person_name;
+ };
+ Map.prototype.inconsistencyOfMap = function(name) {
+ 	var posts_contain_person_name = [];
+ 	for(i=0; i<this._posts.length; i++)
+ 	{
+ 		if(this._posts[i].includes(name))
+ 			posts_contain_person_name.push(this._posts[i]);
+    }
+ if(posts_contain_person_name.length > 1)
+	return true;
+  	return false;
+  };
+
+ Map.prototype.findLocation = function(name) {	
+ 	for(i=0; i<this._posts.length; i++)
+ 	{
+ 		if(this._posts[i].includes(name))
+ 			return true;
+ 	}
+ 	return false;
   };
 
 module.exports = Map;
